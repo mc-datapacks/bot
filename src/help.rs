885847c9392dcf65610d8d3a6a97e948272f7627
oOBoomberOo <boombercmd123@gmail.com>
2020-04-17 01:21:06 +0700

    I don't know anymore

--- src/help.rs
+++ src/help.rs
@@ -7,8 +7,8 @@ use serenity::framework::standard::{help_commands, Args, HelpOptions, CommandGro
 use std::collections::HashSet;
 
 #[help]
-#[strikethrough_commands_tip_in_guild = ""]
 #[lacking_permissions = "Hide"]
+#[wrong_channel = "Strike"]
 fn help_message(ctx: &mut Context, msg: &Message, args: Args, help_options: &'static HelpOptions, groups: &[&'static CommandGroup], owners: HashSet<UserId>) -> CommandResult {
 	help_commands::with_embeds(ctx, msg, args, help_options, groups, owners)
 }
